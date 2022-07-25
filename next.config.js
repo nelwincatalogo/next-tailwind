bb717616d9702604bf3e92846a4fb84054a7046b
Nelwin Catalogo <catalogonelwin@gmail.com>
2022-07-25 14:13:50 +0800

    🔧 remove console logs

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   trailingSlash: true,
+  compiler: {
+    removeConsole: {
+      exclude: ['error'],
+    },
+  },
 };
 
 module.exports = nextConfig;
