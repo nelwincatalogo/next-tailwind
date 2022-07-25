c3f236827b68dd33e04618e108a2d4c4ad10c8e7
Nelwin Catalogo <catalogonelwin@gmail.com>
2022-07-25 13:34:40 +0800

    ✨ implement react-icons and react-alert

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import type { NextPage } from "next";
+import { useAlert } from "react-alert";
+
 import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
+  const alert = useAlert();
+
   return (
     <div>
       <Head>
@@ -13,6 +17,15 @@ const Home: NextPage = () => {
 
       <main>
         <div className="text-red-500 font-bold">Hello World</div>
+        <button
+          onClick={() => {
+            alert.success("Yeyyy!");
+            alert.error("Yeyyy!");
+            alert.info("Yeyyy!");
+          }}
+        >
+          Alert
+        </button>
       </main>
     </div>
   );
